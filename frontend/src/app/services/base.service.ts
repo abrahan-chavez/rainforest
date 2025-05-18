51394d1a883249d9253eaab6db45064a334ef66b
hunter-riot <hmartin@riot.inc>
2025-05-18 15:50:10 -0500

    Automatically push to github pages

--- frontend/src/app/services/base.service.ts
+++ frontend/src/app/services/base.service.ts
@@ -1,11 +1,12 @@
-import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { inject, Injectable } from '@angular/core';
+import { HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root',
 })
 export class BaseService {
-  protected baseUrl = "http://44.210.122.181:5030/";
+  protected baseUrl = environment.apiBaseUri;
   protected httpOptions = {
     headers: new HttpHeaders({
       'Content-Type': 'application/json',
---
+++ frontend/src/environments/environment.deploy.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: true,
+  apiBaseUri: "http://44.210.122.181:5030",
+};
